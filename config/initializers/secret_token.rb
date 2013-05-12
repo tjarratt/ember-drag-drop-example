75b349fdfe32455d1bb16f43788b5b4be166851d
tjarratt@gmail.com <tjarratt@RetinaBook.local>
2013-05-12 13:03:11 -0700

    Initial commit of empty rails app + ember.js

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glass::Application.config.secret_token = 'd7706ae552a2c6e51e5cd0aa8411a5d8690e74f69e95a3f88eef5248888afbaf8bfe4f75be053d0c8765ee0b2980a471a0fdd53cec10f5e45e49c5e88c1f5720'
